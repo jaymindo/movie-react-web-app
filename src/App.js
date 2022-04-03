78fe495f338bf269eeaba872bb6b53f64f429db5
Jaymin Do <81680394+jaymindo@users.noreply.github.com>
2022-04-03 17:12:59 +1000

    movie website react app

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import MovieCard from "./component/MovieCard";
+import SearchIcon from "./search.svg";
+const API_URL = "http://omdbapi.com?apikey=bd95cc0d";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  return (
+    <div className="app">
+      <h1>Netflix Clone</h1>
+
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for movies"
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
